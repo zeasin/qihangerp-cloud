1b117e045b8c1dd464d755bb0b821a6be39fe3fa
启航 <qiliping@yeah.net>
2024-01-29 09:50:16 +0800

    初始化项目

--- Home.py
+++
@@ -1,9 +0,0 @@
-from flet_core import UserControl, Column, Row, FloatingActionButton, icons, TextField, Tabs, Tab, Text, \
-    MainAxisAlignment
-
-
-class HomePage(Column):
-    def __init__(self):
-        super().__init__()
-        self.controls.clear()
-        self.controls.append(Column([Text("Body!")], alignment=MainAxisAlignment.START, expand=True))
